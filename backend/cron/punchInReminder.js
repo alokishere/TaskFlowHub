cc457eb18f2ef28d109443844b95afc0e927106d
alokishere <alokvishwakarmabdh@gmail.com>
2026-04-08 13:39:25 +0530

    chnage

--- backend/cron/punchInReminder.js
+++ backend/cron/punchInReminder.js
@@ -22,7 +22,7 @@ const punchInReminderTask = cron.schedule(
 );
 
 const punchOutReminderTask = cron.schedule(
-  '39 1 * * 1-6',
+  '41 1 * * 1-6',
   async () => {
     try {
       await sendPushToAll({
